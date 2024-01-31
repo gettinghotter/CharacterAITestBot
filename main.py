1b9941b65388d9fc1b5225185446c3f68367de23
gettinghotter <gamingakk19@gmail.com>
2024-01-31 20:29:05 +0300

    Created first bot code

---
+++ main.py
@@ -0,0 +1,31 @@
+import telebot
+import webbrowser
+bot = telebot.TeleBot('6551916291:AAHrq33hbQzlJWy7mubjsNM2laiGrDOM7DA')
+
+
+@bot.message_handler(commands=['developer'])
+def main(message):
+    bot.send_message(message.chat.id,'https://github.com/gettinghotter')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id,f'Привет, {message.from_user.first_name} {message.from_user.last_name}  это бот, который будет делать что-то. Введи /help для помощи')
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, 'Для связи с разработчиком напиши /developer')
+
+@bot.message_handler(commands=['messagecommands'])
+def main(message):
+    bot.send_message(message.chat.id, message)
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower()  == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+
+
+
+bot.infinity_polling(none_stop=True)
