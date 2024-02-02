d9c3176e314e454eb9034105574a365e3f75f3ba
gettinghotter <gamingakk19@gmail.com>
2024-02-02 23:30:14 +0300

    First API connecting. Lots of bugs and API connecting errors.

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+#81da6bdced6150a7fc939ea82403c9c2910f0b3f
 import telebot
 import webbrowser
+
+
+#https://github.com/kramcat/CharacterAI?tab=readme-ov-file
+#https://pycai.gitbook.io/welcome/examples/chatting
+
+
+
+from characterai import PyCAI
+
+client = PyCAI('81da6bdced6150a7fc939ea82403c9c2910f0b3f')
+
+char = input('Enter CHAR: ')
+
+chat = client.chat.get_chat(char)
+
+participants = chat['participants']
+
+if not participants[0]['is_human']:
+    tgt = participants[0]['user']['username']
+else:
+    tgt = participants[1]['user']['username']
+
+while True:
+    message = input('You: ')
+
+    data = client.chat.send_message(
+        chat['external_id'], tgt, message
+    )
+
+    name = data['src_char']['participant']['name']
+    text = data['replies'][0]['text']
+
+    print(f"{name}: {text}")
+
+
 bot = telebot.TeleBot('6551916291:AAHrq33hbQzlJWy7mubjsNM2laiGrDOM7DA')
 
 
